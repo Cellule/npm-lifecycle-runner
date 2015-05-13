8a4c0aa22bfd367a4d02b093de530fea67e338ed
Michael Ferris <mike.ferris@hotmail.com>
2015-05-12 23:29:04 -0400

    base files

---
+++ index.js
@@ -0,0 +1,141 @@
+var isProd = process.env.NODE_ENV !== "production";
+var lifecycleEvent = process.env.npm_lifecycle_event;
+var any = require("lodash.some");
+var exec = require("child_process").exec;
+
+/*
+var supportedLifeCycles = [
+  // only when installing in its own folder `npm install`
+  "devInstall",
+  // when installing for production or from another module
+  "prodInstall",
+  // on every install lifecycle event
+  "install",
+];
+*/
+var defaultLifeCycle = "devInstall";
+
+// TODO:: add support for other lifecycles
+var lifeCycles = [
+  function isDevInstall(projectRoot) {
+    if(
+      !isProd &&
+      lifecycleEvent === "install" &&
+      // installed from this module
+      projectRoot === process.cwd()
+    ) {
+      return "devInstall";
+    }
+  },
+  function isProdInstall(projectRoot) {
+    if(
+      lifecycleEvent === "install" &&
+      (
+        isProd ||
+        // installed from another module
+        projectRoot !== process.cwd()
+      )
+    ) {
+      return "prodInstall";
+    }
+  },
+  function isInstall() {
+    if(lifecycleEvent === "install") {
+      return "install";
+    }
+  }
+];
+
+  // doDevInstall: !isProd && lifecycleEvent === "install"
+function report(msg) {
+  console.warn("npm-lifecycle-runner: " + msg);
+}
+
+function execCmd(cmdDetails, done) {
+  var cmd;
+  var opts = cmdDetails.opts || process;
+  if(typeof cmdDetails === "string") {
+    cmd = cmdDetails;
+    opts = process;
+  } else {
+    cmd = cmdDetails.cmd;
+  }
+  if(!cmd) {
+    report("Empty cmd");
+    return done();
+  }
+
+  exec(cmd, opts, function (err, stdout, stderror) {
+    if(err) {
+      if(cmdDetails.onError) {
+        cmdDetails.onError(err, stdout, stderror);
+        return done();
+      }
+      console.error(stderror);
+      return done();
+    }
+
+    if(cmdDetails.onSuccess) {
+      if(typeof cmdDetails.onSuccess === "string") {
+        console.log(cmdDetails.onSuccess);
+      } else {
+        cmdDetails.onSuccess(stdout, stderror);
+      }
+      return done();
+    }
+    console.log("Success");
+    console.log(stdout);
+    return done();
+  });
+}
+
+module.exports = function(projectRoot, cmds) {
+  if(typeof projectRoot !== "string") {
+    return report("projectRoot missing or is not a string");
+  }
+  if(!cmds) {
+    return report("commands missing");
+  }
+  if(typeof cmds !== "object" || !Array.isArray(cmds)) {
+    cmds = [cmds];
+  }
+
+  var type = any(lifeCycles, function(lifecycle) {
+    return lifecycle(projectRoot);
+  });
+
+  if(!type) {
+    return report("Unsupported lifecycle event");
+  }
+
+  cmds = cmds.map(function(cmd) {
+    var result = cmd || {};
+    if(typeof cmd === "string") {
+      result = {
+        type: defaultLifeCycle,
+        cmd: cmd
+      };
+    }
+    result.type = result.type || defaultLifeCycle;
+    return result;
+  });
+
+  function run(cmdDetails, next) {
+    return function() {
+      process.nextTick(function() {
+        if(type === cmdDetails.type) {
+          execCmd(cmdDetails, next);
+        }
+        next();
+      });
+    };
+  }
+
+  var nextCallback = function(){};
+  for(var i = cmds.length - 1; i >= 0; i--) {
+    var cmd = cmds[i];
+    cmd.run = run(cmd, nextCallback);
+    nextCallback = cmd.run;
+  }
+  cmds[0].run();
+};
